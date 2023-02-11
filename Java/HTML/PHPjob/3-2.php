2432af158fa141e491da4e91e5239540a1fd30a2
Ouchi-Miku <123130348+Ouchi-Miku@users.noreply.github.com>
2023-02-11 12:07:36 +0900

    3−2
    確認をお願いします

--- Java/HTML/PHPjob/3-2.php
+++ Java/HTML/PHPjob/3-2.php
@@ -1,13 +1,14 @@
 
 <?php
-$fruits=["りんごは"=>"300円です。","みかんは"=>"150円です。","ももは"=>"3000円です。"];
+$fruits=["りんごは"=>"apple","みかんは"=>"orange","ももは"=>"peach"];
 
 foreach ($fruits as $key=> $value) {
-    echo $key;
-    echo $value;
-    echo'<br />';
+ echo $key;
+ echo $value;
+ echo'<br />';
 }
 
+$fruits=["りんご", "みかん","もも",];
 echo $fruits[0];
 echo '<br />';
 echo $fruits[1];
@@ -15,7 +16,10 @@ echo '<br />';
 echo $fruits[2];
 echo '<br />';
 
+var_dump($fruits);
 
+function getPrice($fruits,$number) {
 
-
-?>
+    $fee = $fruits * $number;
+    return $fee;
+    }
