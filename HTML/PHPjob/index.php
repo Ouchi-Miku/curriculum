b9c4866d3cc91e5245d2481cdbe0c64269622627
Ouchi-Miku <123130348+Ouchi-Miku@users.noreply.github.com>
2023-02-15 19:37:46 +0900

    練習問題２
    確認をお願いします！

---
+++ HTML/PHPjob/index.php
@@ -0,0 +1,15 @@
+
+    <form action="result.php" method="get">
+    数字入力：<input type="text" name="my_nomber" />
+</br>
+    #include<stdio.h>
+    #include<stdlib.h>   
+    $str = "yoneyama";
+    echo strpos($str, "a");
+    // => 5と出力される
+    <br>
+  
+    <input type="submit" value="占い" />
+    </form>
+
+
